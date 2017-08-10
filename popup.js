33b268c4390f175f393bec0d06fffe6825960463
Hannes hermansson <hanneshermansson0000@gmail.com>
2017-08-11 00:04:17 +0200

    top section remodeled and cleaned up

--- popup.js
+++ popup.js
@@ -1,16 +1,8 @@
 window.onload = function () {
 
-  var editor0 = CodeMirror(document.getElementById('html'), {
-    lineNumbers: false
-  });
-
-  var editor1 = CodeMirror(document.getElementById('css'), {
-    lineNumbers: false
-  });
-
-  var editor2 = CodeMirror(document.getElementById('js'), {
-    lineNumbers: false
-  });
+  var editor0 = CodeMirror(document.getElementById('html'), {lineNumbers: false});
+  var editor1 = CodeMirror(document.getElementById('css'), {lineNumbers: false});
+  var editor2 = CodeMirror(document.getElementById('js'), {lineNumbers: false});
 
 }
 
