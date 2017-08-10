b3d8ac7c15a0970b49b82bed256c1da0e05a2e8a
Hannes hermansson <hanneshermansson0000@gmail.com>
2017-08-10 23:33:36 +0200

    added codemirror for syntax highlighting

--- popup.js
+++ popup.js
@@ -1,6 +1,16 @@
 window.onload = function () {
 
+  var editor0 = CodeMirror(document.getElementById('html'), {
+    lineNumbers: false
+  });
 
+  var editor1 = CodeMirror(document.getElementById('css'), {
+    lineNumbers: false
+  });
+
+  var editor2 = CodeMirror(document.getElementById('js'), {
+    lineNumbers: false
+  });
 
 }
 
