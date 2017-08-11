ee3b4eee9c3d1f0f62643d677ad7d714735de7b9
Hannes hermansson <hanneshermansson0000@gmail.com>
2017-08-11 21:07:11 +0200

    added mirroring

--- popup.js
+++ popup.js
@@ -1,17 +1,49 @@
-window.onload = function () {
+window.onload = function() {
 
-  var editor0 = CodeMirror(document.getElementById('html'), {lineNumbers: false});
-  var editor1 = CodeMirror(document.getElementById('css'), {lineNumbers: false});
-  var editor2 = CodeMirror(document.getElementById('js'), {lineNumbers: false});
+  var sky = "";
 
-}
+  var ifa = document.querySelector('iframe').contentDocument;
 
-function render() {
+  (function initframe() {
 
-}
+    var st = document.createElement("style");
+    ifa.querySelector('head').appendChild(st);
 
-function store() {
+    var sc = document.createElement("script");
+    ifa.querySelector('head').appendChild(sc);
+
+  })()
+
+    var editor0 = CodeMirror(document.getElementById('html'), {
+      lineNumbers: false,
+    });
+    var editor1 = CodeMirror(document.getElementById('css'), {
+      lineNumbers: false,
+      matchBrackets: true,
+      parserfile: "css.js"
+    });
+    var editor2 = CodeMirror(document.getElementById('js'), {
+      lineNumbers: false,
+      matchBrackets: true,
+    });
+
+editor0.on("change", function(cm, change) {
+  ifa.querySelector('body').innerHTML = cm.getValue();
+});
+editor1.on("change", function(cm, change) {
+  ifa.querySelector('style').innerHTML = cm.getValue();
+});
+editor2.on("change", function(cm, change) {
+  eval(cm.getValue());
+});
+
+    function render() {
+
+    }
+
+    function store() {
 
 
 
-}
+    }
+  }
